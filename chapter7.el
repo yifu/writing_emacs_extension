93639f4c7a27520da7ac1b61fea48c9f3686f9fa
Yves Baumes <ybaumes@gmail.com>
2012-11-09 23:26:26 +0100

    Last sections in chapter seven.

--- chapter7.el
+++ chapter7.el
@@ -19,17 +19,41 @@
 (if (null (assq 'myrefill-mode minor-mode-alist))
     (setq minor-mode-alist (cons '(myrefill-mode " Myrefill") minor-mode-alist)))
 
-(defun myrefill (beg end len)
-  "After a change, myrefill the paragrap"
-  (message "Myrefill fun.")
-  (let ((left (if (equal 0 len)
-                  beg ;; In case of an insertion return the start of
-                ;; the insertion.
-                (save-excursion ;; In case of a removing return the
-                  ;; eol of the previous line.
-                  (goto-char beg)
-                  (beginning-of-line 0)
+(defun before-second-word-p (pos)
+  "Is pos begore the second word in its line?"
+  (save-excursion
+    (goto-char pos)
+    (beginning-of-line)
+    (skip-chars-forward "^ ")
+    (skip-chars-forward " ")
+    (< pos (point))))
+
+;; (defun myrefill (beg end len)
+;;   "After a change, myrefill the paragrap"
+;;   (let ((left (if (or (equal 0 len) (not (before-second-word-p beg)))
+;;                   beg ;; In case of an insertion return the start of
+;;                 ;; the insertion.
+;;                 (save-excursion
+;;                   (max
+;;                    (progn
+;;                      (goto-char beg)
+;;                      (backward-paragraph 1)
+;;                      (point))
+;;                    (progn
+;;                      (goto-char beg) ;; eol of the previous line.
+;;                      (beginning-of-line 0)
+;;                      (point)))))))
+;;     (save-excursion
+;;       (message "Myrefill start %d beg %d end %d len %d, start == beg? %S " left beg end len (equal beg left))
+;;       (fill-region left end nil nil t))))
+
+(defun myrefill (start end len)
+  "Refill a paragraph at point."
+  (let ((left (if (zerop len)
+                  start
+                (save-excursion
+                  (goto-char start)
+                  (forward-line -1)
+                  (beginning-of-line)
                   (point)))))
-    (save-excursion
-      (message "Myrefill start %d" left)
-      (fill-region left end nil nil t))))
+    (fill-region left end nil nil t)))
