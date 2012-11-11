6bfd2c33d02f31eccea65b667497ecba52e07c79
Yves Baumes <ybaumes@gmail.com>
2012-11-11 18:22:13 +0100

    Up to section 6, included.

--- quip.el
+++ quip.el
@@ -2,12 +2,16 @@
 (defvar quip-mode-hook nil
   "*List of functions to call when entering Quip mode.")
 
-;; (defvar quip-mode-map nil
-;;   "Keymap for quip major mode.")
+(defvar quip-mode-map nil
+  "Keymap for quip major mode.")
 
-;; (if quip-mode-map
-;;     nil
-;;   (setq quip-mode-map (make-keymap)))
+(if quip-mode-map
+    nil
+  (setq quip-mode-map (make-sparse-keymap))
+  (define-key quip-mode-map (kbd "C-x [") 'backward-quip)
+  (define-key quip-mode-map (kbd "C-x ]") 'forward-quip)
+  (define-key quip-mode-map (kbd "C-x n q") 'narrow-to-quip)
+  (define-key quip-mode-map (kbd "C-c w") 'what-quip))
 
 ;; (defvar quip-mode-syntax-table (make-syntax-table)
 ;;  "Syntax table for quip major mode.")
@@ -15,18 +19,34 @@
 ;; (defvar quip-mode-abbrev-table (make-abbrev-table))
 
 (defun quip-mode ()
-  "Run the quip major mode."
+  "Run the quip major mode.
+Special commands:
+\\{quip-mode-map}"
   (interactive "")
   (kill-all-local-variables)
+
   (setq major-mode 'quip-mode)
   (setq mode-name "Quip")
+
   (make-local-variable 'paragraph-start)
-  (setq paragraph-start "%%\\|[ \t\n\^L]")
   (make-local-variable 'paragraph-separate)
-  (setq paragraph-separate "%%$\\|[ \t\^L]*$]")
   (make-local-variable 'page-delimiter)
+
+  (setq paragraph-start "%%\\|[ \t\n\^L]")
+  (setq paragraph-separate "%%$\\|[ \t\^L]*$]")
   (setq page-delimiter "^%%$")
-  ;;  (use-local-map 'quip-mode-map)
+
+  (use-local-map quip-mode-map)
+
   (run-hooks 'quip-mode-hook))
+
+(defun count-quips ()
+  "Count the quips in the buffer."
+  (interactive)
+  (save-excursion
+    (save-restriction
+      (widen)
+      (goto-char (point-min))
+      (count-matches "^%%$"))))
 
 (provide 'quip)
